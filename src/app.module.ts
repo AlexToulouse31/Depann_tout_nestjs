bc0ab769aac06ccf4d1e528300eef090a51ad7f6
PROENCA Sandrine (SNCF VOYAGEURS / ESV TGV Sud Ouest / BCC TOULOUSE) <7410499r@commun.ad.sncf.fr>
2023-02-02 14:00:58 +0100

    Feat: .env, app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,24 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ReservationModule } from './reservations/reservation.module';
 import { ServicesModule } from './services/services.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, AuthModule, ReservationModule, ServicesModule],
+  imports: [
+    UsersModule, 
+    AuthModule, 
+    ReservationModule, 
+    ServicesModule,
+    ConfigModule.forRoot({ envFilePath: '.env'}),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
