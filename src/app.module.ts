13d5b4b888f952d7ce141741876446901bbb0568
HORDEQUIN Alexandre (SNCF RESEAU / EIC LANGUEDOC ROUSSILLON / NARBONNE) <7809188y@commun.ad.sncf.fr>
2023-02-02 13:51:58 +0100

    feat: services et reservations, .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ReservationModule } from './reservations/reservation.module';
+import { ServicesModule } from './services/services.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, ReservationModule, ServicesModule],
   controllers: [AppController],
   providers: [AppService],
 })
