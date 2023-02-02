342b7ee84cdc406acac946bc0bfe7d6fa5fa0c87
HORDEQUIN Alexandre (SNCF RESEAU / EIC LANGUEDOC ROUSSILLON / NARBONNE) <7809188y@commun.ad.sncf.fr>
2023-02-02 14:10:58 +0100

    merge

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,27 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ReservationModule } from './reservations/reservation.module';
 import { ServicesModule } from './services/services.module';
+import * as dotenv from 'dotenv';
+import { DataSource } from "typeorm"
+import { User } from './users/entities/user.entity';
+import { Reservation } from './reservations/entities/reservation.entity';
+import { Service } from './services/entities/service.entity';
+
+dotenv.config({ path: ".env" });// Permet d'éviter l'erreur "Client Password must be a string"
 
 @Module({
   imports: [UsersModule, AuthModule, ReservationModule, ServicesModule],
+  type: "postgres",
+    username: process.env.DB_USERNAME,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: +process.env.DB_PORT,
+    synchronize: true,
+    logging: false,
+    entities: [User,Service, Reservation],
+    migrations: [],
+    subscribers: [],
   controllers: [AppController],
   providers: [AppService],
 })
